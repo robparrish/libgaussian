e6b26a76462d4576a9d80ec371689bb0fb472982
Justin Turney <justin.turney@gmail.com>
2015-02-19 15:38:37 -0500

    Dipoles

--- src/mints/constants.h
+++ src/mints/constants.h
@@ -8,6 +8,11 @@ namespace constants {
 extern const int max_df__;
 extern const double df__[];
 
+constexpr int ncartesian(int am)
+{
+    return (am >= 0) ? ((((am) + 2) * ((am) + 1)) >> 1) : 0;
+}
+
 }
 
 }
--- src/mints/dipole2c.cc
+++ src/mints/dipole2c.cc
@@ -1,5 +1,6 @@
 #include <math.h>
 #include "int2c.h"
+#include "constants.h"
 
 namespace libgaussian {
 
@@ -7,7 +8,8 @@ DipoleInt2C::DipoleInt2C(
     const std::shared_ptr<SBasisSet>& basis1,
     const std::shared_ptr<SBasisSet>& basis2,
     int deriv) :
-    Int2C(basis1,basis2,deriv)
+    Int2C(basis1,basis2,deriv),
+    recursion_(basis1->max_am()+1, basis2->max_am()+1)
 {
     size_t size;
     if (deriv_ == 0) {
@@ -22,7 +24,93 @@ void DipoleInt2C::compute_shell(
     const SGaussianShell& sh1,
     const SGaussianShell& sh2)
 {
-    throw std::runtime_error("Not Implemented");
+    int ao12;
+    int am1 = sh1.am();
+    int am2 = sh2.am();
+    int nprim1 = sh1.nprimitive();
+    int nprim2 = sh2.nprimitive();
+    double A[3], B[3];
+    A[0] = sh1.x();
+    A[1] = sh1.y();
+    A[2] = sh1.z();
+    B[0] = sh2.x();
+    B[1] = sh2.y();
+    B[2] = sh2.z();
+
+    int ydisp = constants::ncartesian(am1) * constants::ncartesian(am2);
+    int zdisp = ydisp + constants::ncartesian(am1) * constants::ncartesian(am2);
+
+    // compute intermediates
+    double AB2 = 0.0;
+    AB2 += (A[0] - B[0]) * (A[0] - B[0]);
+    AB2 += (A[1] - B[1]) * (A[1] - B[1]);
+    AB2 += (A[2] - B[2]) * (A[2] - B[2]);
+
+    memset(buffer1_, 0, 3 * constants::ncartesian(am1) * constants::ncartesian(am2) * sizeof(double));
+
+    double **x = recursion_.x();
+    double **y = recursion_.y();
+    double **z = recursion_.z();
+
+    for (int p1=0; p1<nprim1; ++p1) {
+        double a1 = sh1.e(p1);
+        double c1 = sh1.c(p1);
+        for (int p2=0; p2<nprim2; ++p2) {
+            double a2 = sh2.e(p2);
+            double c2 = sh2.c(p2);
+            double gamma = a1 + a2;
+            double oog = 1.0/gamma;
+
+            double PA[3], PB[3];
+            double P[3];
+
+            P[0] = (a1*A[0] + a2*B[0])*oog;
+            P[1] = (a1*A[1] + a2*B[1])*oog;
+            P[2] = (a1*A[2] + a2*B[2])*oog;
+            PA[0] = P[0] - A[0];
+            PA[1] = P[1] - A[1];
+            PA[2] = P[2] - A[2];
+            PB[0] = P[0] - B[0];
+            PB[1] = P[1] - B[1];
+            PB[2] = P[2] - B[2];
+
+            double over_pf = exp(-a1*a2*AB2*oog) * sqrt(M_PI*oog) * M_PI * oog * c1 * c2;
+
+            // Do recursion
+            recursion_.compute(PA, PB, gamma, am1+1, am2+1);
+
+            ao12 = 0;
+            for(int ii = 0; ii <= am1; ii++) {
+                int l1 = am1 - ii;
+                for(int jj = 0; jj <= ii; jj++) {
+                    int m1 = ii - jj;
+                    int n1 = jj;
+                    /*--- create all am components of sj ---*/
+                    for(int kk = 0; kk <= am2; kk++) {
+                        int l2 = am2 - kk;
+                        for(int ll = 0; ll <= kk; ll++) {
+                            int m2 = kk - ll;
+                            int n2 = ll;
+
+                            double x00 = x[l1][l2],   y00 = y[m1][m2],   z00 = z[n1][n2];
+                            double x10 = x[l1+1][l2], y10 = y[m1+1][m2], z10 = z[n1+1][n2];
+
+                            double DAx = (x10 + x00*(A[0]-x_)) * y00 * z00 * over_pf;
+                            double DAy = x00 * (y10 + y00*(A[1]-y_)) * z00 * over_pf;
+                            double DAz = x00 * y00 * (z10 + z00*(A[2]-z_)) * over_pf;
+
+                            // Electrons have a negative charge
+                            buffer1_[ao12]       -= (DAx);
+                            buffer1_[ao12+ydisp] -= (DAy);
+                            buffer1_[ao12+zdisp] -= (DAz);
+
+                            ao12++;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 void DipoleInt2C::compute_shell1(
     const SGaussianShell& sh1,
