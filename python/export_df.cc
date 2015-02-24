41e093f3cb158d489fcbb783e67c6744b2213558
rparrish <robparrish@gmail.com>
2015-02-23 23:50:00 -0500

    Wrapping DFERI subclasses into python

--- python/export_df.cc
+++ python/export_df.cc
@@ -7,6 +7,9 @@ using namespace libgaussian;
 using namespace boost::python;
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(df_power_ov, DFERI::metric_power_core, 0, 2)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(aodf_core_ov, AODFERI::compute_ao_task_core, 0, 1)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(aodf_disk_ov, AODFERI::compute_ao_task_disk, 0, 1)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(modf_add_ov, MODFERI::add_mo_task, 3, 5)
 
 void export_df()
 {
@@ -30,4 +33,23 @@ void export_df()
         .def("metric_core", &DFERI::metric_core)
         .def("metric_power_core", &DFERI::metric_power_core, df_power_ov())
         ;
+
+    class_<AODFERI, std::shared_ptr<AODFERI>, bases<DFERI>>("AODFERI", init<
+        const std::shared_ptr<SchwarzSieve>&,
+        const std::shared_ptr<SBasisSet>&
+        >())
+        .def("compute_ao_task_core", &AODFERI::compute_ao_task_core, aodf_core_ov())
+        .def("compute_ao_task_disk", &AODFERI::compute_ao_task_disk, aodf_disk_ov())
+        ;
+
+    class_<MODFERI, std::shared_ptr<MODFERI>, bases<DFERI>>("MODFERI", init<
+        const std::shared_ptr<SchwarzSieve>&,
+        const std::shared_ptr<SBasisSet>&
+        >())
+        .def("clear", &MODFERI::clear)
+        .def("add_mo_task", &MODFERI::add_mo_task, modf_add_ov())
+        .def("compute_mo_tasks_core", &MODFERI::compute_mo_tasks_core)
+        .def("compute_mo_tasks_disk", &MODFERI::compute_mo_tasks_disk)
+        ;
+
 }
