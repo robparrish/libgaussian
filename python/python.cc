5b6d9910b69e11d33f6ab7a4dfe21f18f2084a46
Justin Turney <justin.turney@gmail.com>
2015-02-27 17:09:13 -0500

    Renames libpylightspeed to pylightspeed

--- python/python.cc
+++ python/python.cc
@@ -12,15 +12,15 @@ void export_jk();
 void export_sad();
 void export_solver();
 
-BOOST_PYTHON_MODULE(libpylightspeed)
+BOOST_PYTHON_MODULE(pylightspeed)
 {
-    export_collections();        
-    export_ambit();        
-    export_core();        
-    export_mints();        
-    export_ob();        
-    export_df();        
-    export_jk();        
-    export_sad();        
-    export_solver();        
+    export_collections();
+    export_ambit();
+    export_core();
+    export_mints();
+    export_ob();
+    export_df();
+    export_jk();
+    export_sad();
+    export_solver();
 }
